55409a0bd1a1b569f5f3f13b209908c144c985aa
Ihor Banadiga <banadiga@gmail.com>
2019-09-05 16:35:42 +0300

    express-example

--- express-example/index.js
+++ express-example/index.js
@@ -1,10 +1,8 @@
-const express = require('express');
+const express = require('express')
 
-const app  = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.get('/hello/:user', (req, res) => res.send(`Hello ${req.params.user}!`));
+app.get('/hello/:user', (req, res) => res.send(`Hello ${req.params.user}!`))
 
-app.get('/hello/', (req, res) => res.send(`Hello ${req.query.user}!`));
-
-app.listen(port, () => console.log(`Example app listening on port${port}!`));
+app.listen(port, () => console.log(`Example app listening on port${port}!`))
